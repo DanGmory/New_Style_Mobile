bac7e52240932d150fdbe93307ea7e5da2db8ebd
Daniel Alejandro Acosta <acostacastanedaalejandro15@gmail.com>
2025-09-22 15:43:27 -0500

    Update user.dart

--- new_style_app/lib/pages/user/user.dart
+++ new_style_app/lib/pages/user/user.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../widgets/appbar.dart';
-import '../../models/register_model.dart'; // 🔹 Importamos ApiUser
+import '../../models/register_model.dart'; //  Importamos ApiUser
 
 class UserScreen extends StatelessWidget {
   final ApiUser user;
